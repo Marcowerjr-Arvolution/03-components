390f7727969011b94c128e6acd0ee1a4690e6f8f
Marcos Caballero <marcos@arvolution.com>
2022-10-13 19:36:05 -0600

    Add pages and components
    - Button Page and ion-button component.
    - Card Page and ion-card component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -28,6 +28,16 @@ export class HomePage implements OnInit {
       name: 'Avatar',
       redirectTo: '/avatar',
     },
+    {
+      icon: 'radio-button-off-outline',
+      name: 'Buttons',
+      redirectTo: '/button',
+    },
+    {
+      icon: 'card-outline',
+      name: 'Cards',
+      redirectTo: '/card',
+    },
   ];
 
   constructor() {}
